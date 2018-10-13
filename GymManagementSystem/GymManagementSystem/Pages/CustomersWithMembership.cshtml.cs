323fa290a9cefc3518a5e85b6cc857c7d05ee45b
ranepr <ranepr@mail.uc.edu>
2018-10-13 02:30:22 -0400

    Updated Customers page data driven page to relate with membership

---
+++ GymManagementSystem/GymManagementSystem/Pages/CustomersWithMembership.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GymManagementSystem.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementSystem.Pages
+{
+    public class CustomersWithMembership : PageModel
+    {
+        private GymDbContext _context;
+        
+        public CustomersWithMembership(GymDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Tuple<string, List<Models.Customer>>> Customers { get; set; }
+
+        public void OnGet()
+        {
+            Customers = _context.Customers
+                 .Include(c => c.Membership)
+                 .Include(c => c.Schedule)
+                 .GroupBy(s => new { s.Membership.MembershipType })
+                 .OrderBy(c => c.Key.MembershipType)
+                 .Select(c => Tuple.Create(c.Key.MembershipType, c.ToList())).ToList();
+        }
+    }
+}
